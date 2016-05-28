861dbe5dc0a5b542017d67b234afae3bae5b2c23
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-28 12:43:39 -0400

    styling

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+$(function() {
+  //click on a circle and make it rubberBand
+  $('.circle').click(function() {
+    $(this).addClass('animated rubberBand');
+    setTimeout(function () {
+          $('.circle').removeClass('animated rubberBand');
+        }, 1000);
+  });
+//click on the square and make it bounce
+$('.square').click(function() {
+  $(this).addClass('animated bounce');
+  setTimeout(function () {
+        $('.square').removeClass('animated bounce');
+      }, 1000);
+});
 
+//click on a triangle and make it wobble
+$('.triangle').click(function() {
+  $(this).addClass('animated wobble');
+  setTimeout(function () {
+        $('.triangle').removeClass('animated wobble');
+      }, 1000);
+});
+
+})
